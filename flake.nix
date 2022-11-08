3b7c29ea311032f8c2ac810d4189b5c89ee3dac7
Chris Oboe <chris@oboe.email>
2022-11-08 21:16:35 +0100

    try to fix tidal-dl

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         packages.vlmcsd = pkgs.callPackage ./vlmcsd/default.nix {};
         packages.aigpy = pkgs.python3Packages.callPackage ./aigpy/default.nix {};
         packages.lyricsgenius = pkgs.python3Packages.callPackage ./lyricsgenius/default.nix {};
-        packages.tidal-dl = pkgs.python3Packages.callPackage ./tidal-dl/default.nix {};
+        packages.tidal-dl = pkgs.python3Packages.callPackage ./tidal-dl/default.nix {aigpy = packages.aigpy;};
         packages.chicago95 = pkgs.callPackage ./chicago95/default.nix {};
         packages.se98 = pkgs.callPackage ./se98/default.nix {};
         packages.redmond97 = pkgs.callPackage ./redmond97/default.nix {};
