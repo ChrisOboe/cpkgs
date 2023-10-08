50fa4ac4fb2f375db77931bcc063adf248f59e5c
Chris Oboe <chris@oboe.email>
2023-10-08 12:36:27 +0200

    update to stable version of rkvm

--- rkvm/default.nix
+++ rkvm/default.nix
@@ -2,24 +2,24 @@
   lib,
   fetchFromGitHub,
   rustPlatform,
-  openssl_1_1,
+  openssl,
   pkg-config,
   libevdev,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "rkvm";
-  version = "20210117"; # the latest release is broken and doesn't build; use master
+  version = "0.4.1";
 
   src = fetchFromGitHub {
     owner = "htrefil";
     repo = pname;
-    rev = "bf133665eb446d9f128d02e4440cc67bce50f666";
-    hash = "sha256-naWoLo3pPETkYuW4HATkrfjGcEHSGAAXixgp1HOlIcg=";
+    rev = version;
+    hash = "sha256-NJ9wvG3UN80OMKQqrOwcdhsypNdJlQB3SaI/wP0DcHM=";
   };
-  cargoHash = "sha256-7h+/ombiVcBeX60toakFklKi8GalS3v/csn1wkXJJk4=";
+  cargoHash = "sha256-HvHGOcGKHCuyhd2S4BPketGwo3/Av1WRgbAqBHdP9kw=";
 
   nativeBuildInputs = [pkg-config rustPlatform.bindgenHook];
-  buildInputs = [libevdev openssl_1_1];
+  buildInputs = [libevdev openssl];
 
   doCheck = false; # tests are broken and don't build.
 
