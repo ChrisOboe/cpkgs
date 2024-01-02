e17a0186f1b82b8f0bc3390bcca9e420ffe64d51
Chris Oboe <chris@oboe.email>
2024-01-02 01:32:26 +0100

    rename vendorsha to vendorhash

--- ha-mqtt-iot/default.nix
+++ ha-mqtt-iot/default.nix
@@ -15,7 +15,7 @@ buildGoModule rec {
     rev = "${version}";
     sha256 = "sha256-s9fLf6HdPCpXaS31nRvqyCeH+oHFyKHaC6Z3brx0TnI=";
   };
-  vendorSha256 = "sha256-fgHOsw3TCeEqm04ynI4R09xwo9QAjfoyYdwQU0AOCKk=";
+  vendorHash = "sha256-fgHOsw3TCeEqm04ynI4R09xwo9QAjfoyYdwQU0AOCKk=";
 
   meta = {
     description = " A simple configurable MQTT client that allows actions to be taken or values be reported from a PC ";
