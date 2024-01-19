babb9eeb6635e29ae825a662cbe76f99deea3441
Chris Oboe <chris@oboe.email>
2024-01-19 20:59:43 +0100

    fix streamrip

--- streamrip/default.nix
+++ streamrip/default.nix
@@ -12,7 +12,12 @@
   simple-term-menu,
   pick,
   pillow,
+  rich,
+  click-help-colors,
   deezer-py,
+  pytest-asyncio,
+  pytest-mock,
+  aiolimiter,
   pycryptodomex,
   cleo,
   appdirs,
@@ -23,22 +28,23 @@
 }:
 buildPythonPackage rec {
   pname = "streamrip";
-  version = "1.9.7";
+  version = "2.0.2";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "nathom";
     repo = "streamrip";
     rev = "v${version}";
-    sha256 = "sha256-R3b3eIcTzEqx8YPvwAF3GpLKqBBqtKnGx8U88LXJRk4=";
+    sha256 = "sha256-IBZ8wqO0rnfwJi9/rJaB/nis68u98CA/q4rSIqa+nzE=";
   };
 
   patchPhase = ''
     substituteInPlace pyproject.toml \
-      --replace 'cleo = {version = "1.0.0a4", allow-prereleases = true}' 'cleo = "*"' \
-      --replace 'deezer-py = "1.3.6"' 'deezer-py = "^1.3.6"' \
-      --replace 'tomlkit = "^0.7.2"' 'tomlkit = "^0"' \
-      --replace 'aiofiles = "^0.7.0"' 'aiofiles = "*"' \
+      --replace 'Pillow = "^9.0.0"' 'Pillow = "*"' \
+      --replace 'aiofiles = "^0.7"' 'aiofiles = "*"' \
+      --replace 'deezer-py = "1.3.6"' 'deezer-py = "*"' \
+      --replace 'pathvalidate = "^2.4.1"' 'pathvalidate = "*"' \
+      --replace 'tomlkit = "^0.7.2"' 'tomlkit = "*"' \
       --replace 'm3u8 = "^0.9.0"' 'm3u8 = "*"'
   '';
 
@@ -50,6 +56,7 @@ buildPythonPackage rec {
     tqdm
     tomlkit
     pathvalidate
+    rich
     simple-term-menu
     pick
     pillow
@@ -57,10 +64,14 @@ buildPythonPackage rec {
     pycryptodomex
     cleo
     appdirs
+    click-help-colors
     m3u8
     aiofiles
+    aiolimiter
     aiohttp
     aiodns
+    pytest-asyncio
+    pytest-mock
   ];
 
   meta = with lib; {
