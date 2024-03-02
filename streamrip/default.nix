d29fc2d075c9ecaf0c188c3c08fdc51c20cd051f
Chris Oboe <chris@oboe.email>
2024-03-02 22:06:14 +0100

    update streamrip

--- streamrip/default.nix
+++ streamrip/default.nix
@@ -28,24 +28,24 @@
 }:
 buildPythonPackage rec {
   pname = "streamrip";
-  version = "2.0.2";
+  version = "2.0.5";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "nathom";
     repo = "streamrip";
     rev = "v${version}";
-    sha256 = "sha256-IBZ8wqO0rnfwJi9/rJaB/nis68u98CA/q4rSIqa+nzE=";
+    sha256 = "sha256-KwMt89lOPGt6nX7ywliG/iAJ1WnG0CRPwhAVlPR85q0=";
   };
 
   patchPhase = ''
     substituteInPlace pyproject.toml \
-      --replace 'Pillow = "^9.0.0"' 'Pillow = "*"' \
-      --replace 'aiofiles = "^0.7"' 'aiofiles = "*"' \
-      --replace 'deezer-py = "1.3.6"' 'deezer-py = "*"' \
-      --replace 'pathvalidate = "^2.4.1"' 'pathvalidate = "*"' \
-      --replace 'tomlkit = "^0.7.2"' 'tomlkit = "*"' \
-      --replace 'm3u8 = "^0.9.0"' 'm3u8 = "*"'
+      --replace-fail 'aiofiles = "^0.7"' 'aiofiles = "*"' \
+      --replace-fail 'deezer-py = "1.3.6"' 'deezer-py = "*"' \
+      --replace-fail 'm3u8 = "^0.9.0"' 'm3u8 = "*"' \
+      --replace-fail 'pathvalidate = "^2.4.1"' 'pathvalidate = "*"' \
+      --replace-fail 'pytest-asyncio = "^0.21.1"' 'pytest-asyncio= "*"' \
+      --replace-fail 'tomlkit = "^0.7.2"' 'tomlkit = "*"'
   '';
 
   propagatedBuildInputs = [
